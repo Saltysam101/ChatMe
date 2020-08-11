9ea77380fbd25e6f0257ca2125dd4d38ecfbd621
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2020-08-11 14:49:05 -0400

    added routes and authentication

--- src/App.js
+++ src/App.js
@@ -12,12 +12,84 @@ import Login from './pages/Login';
 import { auth } from '/services/firebase';
 import './App.css';
 
-function App() {
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
   return (
-    <div className="App">
-  
-    </div>
-  );
+    <Route
+      {...rest}
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+    />
+  )
+};
+
+
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === false
+        ? <Component {...props} />
+        : <Redirect to='/chat' />}
+    />
+  )
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        })
+      }
+    })
+  }
+
+  render(){
+    return this.state.loading === true ? <h2>Loading...</h2> : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute 
+          path="/chat" 
+          authenticated={this.state.authenticated} 
+          component={Chat}>
+          </PrivateRoute>
+          <PublicRoute 
+          path="/signup" 
+          authenticated={this.state.authenticated} 
+          component={Signup}>
+          </PublicRoute>
+          <PublicRoute 
+          path="/login" 
+          authenticated={this.state.authenticated} 
+          component={Login}>
+          </PublicRoute>
+        </Switch>
+      </Router>
+    )
+  } 
+
 }
+
+
+
+
 
 export default App;
